de6f9fa1437ff787c54a08fc11a8cf0d3025a753
Patrick Wolf <patrick.wolf@pointsource.com>
2014-12-17 10:14:08 -0500

    Add example of custom log levels

--- examples/helloworld/services/random.js
+++ examples/helloworld/services/random.js
@@ -6,17 +6,20 @@ exports.metadata = {
 }
 
 var request = require('request');
-var url;
+var url, logger;
 
 exports.init = function(server, callback) {
 
     var min = server.config.get('randomizer').min || 0;
     var max = server.config.get('randomizer').max || 100;
     url = 'http://www.random.org/integers/?num=1&min=' + min + '&max=' + max + '&col=1&base=10&format=plain&rnd=new';
+    logger = server.logger;
     callback();
 }
 
 exports.get = function(callback) {
+    //custom log level
+    logger.useful('request URL:  ' +  url);
     request.get({url: url}, function(err, response, body) {
         callback(Number(body));
     });
