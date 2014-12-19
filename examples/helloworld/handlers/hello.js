f903943a9afeac708bbe68c18682cad25f398952
Patrick Wolf <patrick.wolf@pointsourcellc.com>
2014-12-19 08:11:45 -0500

    Rewrite express as a service

--- examples/helloworld/handlers/hello.js
+++ examples/helloworld/handlers/hello.js
@@ -1,11 +1,11 @@
 
-module.exports.init = function(app, server, done) {
+module.exports.init = function(server, express, done) {
 
-    app.get('/hello', function(req, res) {
+    //Default is one of the app's defined in the config
+    express.default.get('/hello', function(req, res) {
         server.randomizer.get(function(number) {
             res.status(200).send('hello world, here is a random number!...' + number);
         });
-
     });
 
     done();
--- services/express.js
+++ services/express.js
@@ -1,12 +1,77 @@
 exports.metadata = {
     id: "express",
-    description: "Logging service",
-    dependencies: ['config']
+    description: "ExpressJS service",
+    dependencies: ['config', 'logger']
 };
 
-var _ = require('lodash');
+var _ = require('lodash'),
+    express = require('express'),
+    path = require('path'),
+    async = require('async'),
+    fs = require('fs');
+
+var logger = null;
+
 
 exports.init = function(server, cfg, callback) {
+    var httpConf = server.config.get('http');
+    logger = server.logger;
 
-    callback();
+    var appMap = {};
+
+    async.each(_.keys(cfg), function(appName, appCallback) {
+        var app = express();
+        exports[appName] = app;
+        appCallback();
+    }, function(err) {
+        if (err) {
+            callback(err);
+        } else {
+
+            registerEndpoints(server, function(err) {
+                if (err) {
+                    callback(err);
+                } else {
+                    startExpress(cfg, callback);
+                }
+            });
+
+        }
+    });
+
 };
+
+function registerEndpoints(server, callback) {
+    var handlerDir = path.join(process.cwd(), 'handlers');
+    var files = fs.readdirSync(handlerDir);
+    async.each(files, function (file, initCallback) {
+        if (path.extname(file) === '.js') {
+            logger.debug('Begin initializing handler ' + file);
+            var mod = require(path.resolve(handlerDir, file));
+            mod.init(server, module.exports, function () {
+                logger.debug('Finish initializing handler ' + file);
+                initCallback();
+            });
+        } else {
+            initCallback();
+        }
+    }, function (err) {
+        callback(err);
+    });
+}
+
+function startExpress(cfg, callback) {
+    async.each(_.keys(cfg), function(appName, appCallback) {
+        var configForApp = cfg[appName];
+        var server = exports[appName].listen(configForApp.port, function () {
+            var host = server.address().address;
+            var port = server.address().port;
+            logger.info('App \'%s\' is listening at http://%s:%s', appName, host, port);
+            appCallback();
+        }).on('error', function(err) {
+            appCallback(err);
+        });
+    }, function(err) {
+        callback(err);
+    });
+}
