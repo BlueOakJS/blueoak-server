bec8f92af703aedb4bbe05fe0afeccf1367c960e
Patrick Wolf <patrick.wolf@pointsource.com>
2014-12-24 12:17:52 -0500

    Adding session support

--- examples/helloworld/handlers/hello.js
+++ examples/helloworld/handlers/hello.js
@@ -3,8 +3,12 @@ module.exports.init = function(server, express, done) {
 
     //Default is one of the app's defined in the config
     express.default.get('/hello', function(req, res) {
+        var rcount = req.session.rcount || 0;
+        rcount += 1;
+        req.session.rcount = rcount;
         server.get('randomizer').get(function(number) {
-            res.status(200).send('hello world, here is a random number!...' + number);
+            res.status(200).send('hello world, here is a random number!...' + number + '.' +
+            '  You have used this service ' + rcount + ' times.');
         });
     });
 
@@ -18,6 +22,9 @@ module.exports.init = function(server, express, done) {
         res.status(200).send('PUT worked');
     });
 
+    express.default.get('/', function(req, res) {
+        res.status(200).send('GET worked');
+    });
 
 
     express.admin.get('/admin', function(req, res) {
---
+++ middleware/session.js
@@ -0,0 +1,38 @@
+/*
+ * Use the node-cors library to enable CORS on all express.js apps and endpoints.
+ *
+ * All config options are passed directly to the cors library.
+ * See https://github.com/troygoode/node-cors#configuration-options
+ */
+
+exports.metadata = {
+    id: "session",
+    description: "Enables an HTTP session"
+};
+
+var session = require('cookie-session'),
+    _ = require('lodash');
+
+var logger = null;
+
+exports.init = function(server, apps, cfg, callback) {
+
+    logger = server.get('logger');
+    if (cfg.type === 'cookie') {
+
+        var keys = cfg.keys;
+        if (!keys) {
+            logger.error('Cooke sessions require a key to be set.');
+        } else {
+            _.keys(apps).forEach(function(appName) {
+                apps[appName].use(session({
+                    keys: keys
+                }));
+                logger.debug('Added session to ' + appName);
+            });
+        }
+    } else {
+        logger.error('Unknown session type', cfg.type);
+    }
+    callback();
+};
