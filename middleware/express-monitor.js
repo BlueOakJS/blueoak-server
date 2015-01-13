b3144f0b9e4477920a7ddda58183b9dfe603a672
Patrick Wolf <patrick.wolf@pointsourcellc.com>
2015-01-13 11:52:20 -0500

    Switch to node-statsd for stats

--- middleware/express-monitor.js
+++ middleware/express-monitor.js
@@ -2,7 +2,7 @@ exports.init = function(app, config, logger, monitor) {
     var cfg = config.get('express-monitor');
     if (monitor.enabled()) {
         logger.debug('Enabled express monitor.');
-        app.use(monitor.getExpressHelper(cfg.prefix, { timeByUrl: true }));
+        app.use(monitor.express(cfg.prefix, true));
     }
 
 }
