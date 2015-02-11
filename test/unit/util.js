3362f1c2d0ea473f8587299b08f83f14e6c39a70
Patrick Wolf <patrick.wolf@pointsource.com>
2015-02-11 10:02:05 -0500

    Improve some of the cache tests

---
+++ test/unit/util.js
@@ -0,0 +1,59 @@
+//Some utilities to help with testing services
+
+var di = require('../../lib/di'),
+    _ = require('lodash');
+
+//creates a mock config service
+function getConfigService(cfg) {
+    return {
+        get: function(id) {
+            return cfg[id] || {};
+        }
+    };
+}
+
+//create a mock logger
+var logger = {};
+
+['silly', 'debug', 'verbose', 'info', 'warn', 'error'].forEach(function(method) {
+    logger[method] = function() {}; //no-op
+});
+
+
+//This will simulate the normal dependency injection
+//pass in a service module, a set of config to inject into the config service,
+//and an optional mapping of additional injections
+//config and logger are handled automatically, anything else needs to be included in the injections map
+//callback is optional as well
+exports.initService = function(module, config, injections, callback) {
+    //injections is optional
+    if (!injections) {
+        injections = {};
+    }
+
+    if (_.isFunction(injections)) {
+        callback = injections;
+        injections = {};
+    }
+
+    if (!callback) {
+        callback = function() {};
+    }
+
+    injections.config = getConfigService(config);
+    injections.logger = logger;
+
+    var args = [];
+    di.getParamNames(module.init).forEach(function(name) {
+        args.push(injections[name]);
+    });
+
+    if (di.hasCallback(module.init)) {
+        args.push(callback);
+        module.init.apply(null, args);
+    } else {
+        module.init.apply(null, args);
+        callback();
+    }
+
+}
