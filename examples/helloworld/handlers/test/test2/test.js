42196d98d3710aa035f03ad1b2c66e565a3abb44
Patrick Wolf <patrick.wolf@pointsourcellc.com>
2015-02-17 09:26:00 -0500

    Revert context root changes

--- examples/helloworld/handlers/test/test2/test.js
+++
@@ -1,9 +0,0 @@
-//all the endpoints here are exposed through /test/test2
-
-module.exports.init = function(app) {
-    app.get('/hello', function (req, res) {
-
-        res.status(200).json({hello: "world"});
-    });
-
-}
--- services/middleware.js
+++ services/middleware.js
@@ -5,7 +5,6 @@
 var express = require('express');
 
 var app = null;
-var subAppMap = {}; //map a route, e.g. /test to the express subapp that handles it
 
 exports.init = function(serviceLoader, callback) {
 
@@ -14,20 +13,6 @@ exports.init = function(serviceLoader, callback) {
     callback();
 }
 
-/**
- * Get an express app.  If a route path is given, return a subapp for the path, otherwise return the main app.
- * @param routePath a path of a subapp, like '/test'
- * @returns {*}
- */
-exports.getApp = function(routePath) {
-    if (routePath) {
-        var subApp = subAppMap[routePath];
-        if (!subApp) {
-            subApp = express();
-            app.use(routePath, subApp);
-            subAppMap[routePath] = subApp;
-        }
-        return subApp;
-    }
+exports.getApp = function() {
     return app;
 }
