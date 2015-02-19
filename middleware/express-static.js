639709143ecc67c7c9deb0b9583b0f5a7822902b
ErikD <erik.daughtrey@pointsourcellc.com>
2015-02-19 07:41:26 -0500

    Small change to allow using www or docs key to config documents directory of express-static

--- middleware/express-static.js
+++ middleware/express-static.js
@@ -6,9 +6,9 @@
  *
  * Add a JSON block to the application default.json with the following:
  *    "express-static" : {
- *       "docs": "./www"
+ *       "www": "./mywwwdir"
  *    }
- *    This will serve the static content from the www directory relative to the default
+ *    This will serve the static content from the mywwwdir directory relative to the default
  *    application server directory.
  *
  * To enable: add "express-static" to the middleware list of services to load in default.json.
@@ -21,12 +21,15 @@ var logger = null;
 
 exports.init = function(app, config, logger) {
   var cfg = config.get('express-static');
-  if (!cfg.docs) {
+  var cfgDir = cfg.www;
+  if(!cfgDir) {
+    cfgDir = cfg.docs;
+  }
+  if (!cfgDir) {
     logger.warn('No document root is configured for express-static.');
   } else {
-    var docsDir = path.resolve(global.__appDir, cfg.docs);
-    logger.debug('Enabled static file hosting.');
-    logger.info('Serving content from: %s.', docsDir);
+    var docsDir = path.resolve(global.__appDir, cfgDir);
+    logger.info('Serving static content from: %s.', docsDir);
     app.use(es(docsDir));
   }
 };
