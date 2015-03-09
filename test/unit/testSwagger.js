6edddc77a0614c1b36dd812958d07a162d269c4c
Patrick Wolf <patrick.wolf@pointsource.com>
2015-03-09 10:18:44 -0400

    Add numerical validation

--- test/unit/testSwagger.js
+++ test/unit/testSwagger.js
@@ -17,6 +17,11 @@ describe('String validation test', function () {
 
         assert.equal(swaggerUtil.validateParameterType({
             type: 'string',
+            maxLength: 0
+        }, '1').status, 'failed');
+
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'string',
             maxLength: 8
         }, '123456789').status, 'failed');
     });
@@ -53,6 +58,77 @@ describe('String validation test', function () {
             type: 'string',
             pattern: '^[0-9]{1,3}$'
         }, '1000').status, 'failed');
+    });
+
+});
+
+
+describe('Number validation test', function () {
+
+    it('Validate multipleOf is honored', function () {
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            multipleOf: 2
+        }, 4).status, 'success');
+
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            multipleOf: 3
+        }, 7).status, 'failed');
+    });
+
+    it('Validate maximum is honored', function () {
+
+        //implicitly inclusive
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            maximum: 2
+        }, 2).status, 'success');
+
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            maximum: 2,
+            exclusiveMaximum: false
+        }, 2).status, 'success');
+
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            maximum: 20,
+        }, 10).status, 'success');
+
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            exclusiveMaximum: true,
+            maximum: 2
+        }, 2).status, 'failed');
+
+    });
+
+    it('Validate minimum is honored', function () {
+
+        //implicitly inclusive
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            minimum: 2
+        }, 2).status, 'success');
+
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            minimum: 2,
+            exclusiveMinimum: false
+        }, 2).status, 'success');
+
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            minimum: 20,
+        }, 30).status, 'success');
+
+        assert.equal(swaggerUtil.validateParameterType({
+            type: 'number',
+            exclusiveMinimum: true,
+            minimum: 2
+        }, 2).status, 'failed');
+
     });
 
 });
