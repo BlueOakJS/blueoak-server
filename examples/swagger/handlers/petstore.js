b8295e89a7b2571ee66cf91522ca96be904b9a0b
Patrick Wolf <patrick.wolf@pointsourcellc.com>
2015-03-23 16:01:00 -0400

    move swagger example into separate project

--- examples/swagger/handlers/petstore.js
+++ examples/swagger/handlers/petstore.js
@@ -1,12 +1,16 @@
+
 exports.findPets = function(req, res, next) {
     res.status(200).json({});
-}
+};
 
 exports.findPetById = function(req, res, next) {
     res.status(200).json({});
-}
+};
 
 exports.addPet = function(req, res, next) {
-    console.log(req.body);
     res.status(201).json({});
-}
+};
+
+exports.deletePet = function(req, res, next) {
+    res.status(200).json({});
+};
