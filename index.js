35b220a4847d2d2dc0502d1aaad6de8962aa9720
Patrick Wolf <patrick.wolf@pointsource.com>
2015-04-03 17:10:48 +0000

    Fix occasional issues with trying to print version number during startup

--- index.js
+++ index.js
@@ -253,7 +253,6 @@ function initServices(opts, callback) {
 }
 
 function printVersion(logger) {
-    var packageFile = path.resolve(__dirname, 'package.json');
-    var json = JSON.parse(fs.readFileSync(packageFile));
-    logger.info('Starting %s v%s', json.name, json.version);
+    var pkg = require('./package.json')
+    logger.info('Starting %s v%s', pkg.name, pkg.version);
 }
