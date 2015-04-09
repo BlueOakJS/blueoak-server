984dfe613252b770ecce74345b98f209b0f912dc
Patrick Wolf <patrick.wolf@pointsourcellc.com>
2015-04-09 15:03:07 -0400

    Add test case for declarative routes

---
+++ test/integration/fixtures/server2/handlers/camelCase.js
@@ -0,0 +1,6 @@
+/* Copyright © 2015 PointSource, LLC. All rights reserved. */
+
+module.exports.get = function(req, res) {
+    console.log("GET!");
+    res.status(200).json({name: 'endpoint4'});
+}
