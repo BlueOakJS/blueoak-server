c8f4d0654be3264ab1c26b7a8ed364112ab795bc
Patrick Wolf <patrick.wolf@pointsource.com>
2015-04-21 11:33:09 -0400

    Add a way to configure a logger via code rather than config

---
+++ examples/custom-logging/logger.js
@@ -0,0 +1,12 @@
+//Placing a logger.js in my app root let's me setup a logger via code rather than config.
+var winston = require('winston');
+
+//From within the init, I'm free to setup whatever transports I want
+module.exports.init = function(logger) {
+
+    logger.add(winston.transports.Console, {
+        timestamp: function() {
+            return Date.now();
+        }
+    });
+}
