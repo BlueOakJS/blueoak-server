19266467ed025f6c62aa0b8b985aaf4a21a4f61f
Patrick Wolf <patrick.wolf@pointsource.com>
2015-05-14 16:25:59 +0000

    #13 - Experimenting with some sinon-based unit tests

--- test/unit/util.js
+++ test/unit/util.js
@@ -2,7 +2,8 @@
 //Some utilities to help with testing services
 
 var di = require('../../lib/di'),
-    _ = require('lodash');
+    _ = require('lodash')
+    sinon = require('sinon');
 
 //creates a mock config service
 function getConfigService(cfg) {
@@ -14,17 +15,10 @@ function getConfigService(cfg) {
 }
 
 //create a mock logger
-var logger = {};
-
-['silly', 'debug', 'verbose', 'info', 'warn', 'error'].forEach(function(method) {
-    logger[method] = function() {}; //no-op
-});
+var logger = require('./mocks/logger');
 
 //create a mock monitor
-var monitor = {};
-['increment', 'decrement', 'set', 'unique', 'gauge', 'histogram', 'timing'].forEach(function(method) {
-    monitor[method] = function() {}; //no-op
-});
+var monitor = require('./mocks/monitor');
 
 //This will simulate the normal dependency injection
 //pass in a service module, a set of config to inject into the config service,
@@ -63,4 +57,59 @@ exports.initService = function(module, config, injections, callback) {
         callback();
     }
 
+}
+
+
+/* This is a new init method to replace initService
+ * mod: the module to init
+ * serviceMap (optional) - a map of name to module
+ * serviceCallback - a callback once the service stubs have been loaded to give
+ *   an opportunity to stub out the methods calls
+ * callback - a callback once init has completed
+ */
+exports.init = function(mod, serviceMap, serviceCallback, callback) {
+    if (!callback) { //no opts
+        callback = serviceCallback;
+        serviceCallback = serviceMap;
+        serviceMap = {};
+    }
+    
+    global.services = {
+      get: function(name) {
+          console.log("Get", name)
+          return serviceMap[name];
+      }  
+    };
+    
+    var paramNames = di.getParamNames(mod.init);
+    var mockParams = [];
+    paramNames.forEach(function(paramName) {
+        if (!serviceMap[paramName]) {
+            
+            //since it's possible that this has already been loaded once and
+            //has stubbed out methods, delete it from the cache
+            var modPath = require.resolve('./mocks/' + paramName);
+            delete require.cache[modPath];
+            
+            serviceMap[paramName] = require('./mocks/' + paramName);
+        }
+        mockParams.push(serviceMap[paramName]);
+    });
+    serviceCallback(serviceMap);
+    if (di.hasCallback(mod.init)) {
+        mockParams.push(callback);
+        mod.init.apply(global, mockParams);
+    } else {
+        var thrownError = null;
+        try {
+            mod.init.apply(global, mockParams);
+        } catch (err) {
+            thrownError = err;
+        }
+        callback(thrownError);
+    }
+}
+
+exports.restore = function() {
+    
 }
