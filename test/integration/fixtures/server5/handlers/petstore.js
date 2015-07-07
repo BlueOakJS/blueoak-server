dd9b161502c6d6e107044044cf68608fdde80b66
Patrick Wolf <patrick.wolf@pointsource.com>
2015-07-07 12:58:41 +0000

    Fix #26 - allow custom MW functions on swagger endpoints

---
+++ test/integration/fixtures/server5/handlers/petstore.js
@@ -0,0 +1,25 @@
+/* Copyright © 2015 PointSource, LLC. All rights reserved. */
+exports.pets1 = function(req, res, next) {
+    res.json({
+        name: 'pets1'
+    })
+};
+
+exports.pets2 = function(req, res, next) {
+    res.json({
+        name: 'pets2'
+    })
+};
+
+exports.pets3 = function(req, res, next) {
+    res.json({
+        name: 'pets3'
+    })
+};
+
+exports.pets4 = function(req, res, next) {
+    res.json({
+        name: 'pets4'
+    })
+};
+
