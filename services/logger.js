f78417d00d50fdc003d26269a3db093ab1b3be67
ErikD <erik.daughtrey@pointsourcellc.com>
2015-09-15 10:22:18 -0400

    added timestamp function capability and component loggers

--- services/logger.js
+++ services/logger.js
@@ -77,6 +77,41 @@ exports.init = function(config) {
         };
     });
 
+    module.exports['components'] = cfg.components;
+    module.exports['logger'] = logger;
+    module.exports['levels'] = cfg.levels;
+    module.exports['getComponentLogger'] = function(component) {
+        var theLogger = this.logger;
+        if(!this.components || !this.components[component] || !this.components[component].loglevels) {
+            theLogger.error('The component logger ' + component + ' is not configured or not configured properly. Using base logger.');
+            return theLogger;
+        }
+        var loglevels = this.components[component].loglevels;
+        var logger = this;
+        var tmp = {'component':component};
+        var self = this;
+        _.keys(this.levels).forEach(function(lev){
+            tmp[lev] = function() {
+                var args = [].slice.call(arguments);
+                args[0] = "" + this.component + ' - ' + args[0];
+                tlevels = [];
+                //look through transports and save current log levels
+                Object.keys(theLogger.transports).forEach(function(k) {
+                    tlevels.push([k, theLogger.transports[k].level]);
+                    var newLevel = loglevels[k];
+                    if(newLevel) { // if overridden by component, set the loglevel for this transport
+                        theLogger.transports[k].level = newLevel;
+                    }
+                });
+                self[lev](args);
+                tlevels.forEach(function(levelinfo) { // restore all the transport loglevels
+                    theLogger.transports[levelinfo[0]].level = levelinfo[1];
+                });
+            }
+        });
+        return tmp;
+    };
+
     //The buffer stores all logged data, regardless of log level
     //rather than relying on array.push/slice which are slow,
     //we keep the buffer a fixed size and just loop the index around
@@ -122,25 +157,88 @@ exports.init = function(config) {
 };
 
 
-//Determine the name of the service that called logger
+//Determine the name of the service or javascript module that called logger
 function getLocation() {
     var trace = stackTrace.get();
 
-    //use index 2 because 0 is the call to getLocation, and 1 is the call to logger
-    var mod = require(trace[2].getFileName());
-    if (mod) { //__id is injected into services by the loader
-        return mod.__id;
+    //trace.forEach(function(e){
+    //    console.log('mytrace: ' + e.getFileName());
+    //});
+
+    //use (start at) index 2 because 0 is the call to getLocation, and 1 is the call to logger
+    //  However, component loggers put an extra level in, so search down until the location is not 'logger'
+    //  There's code to search a long way down the stack, but in practice the next (3) will be hit and used
+    //    since its name is not logger.
+    var idx = 2;
+    var modNm = 'logger';
+    var rmodNm = null;
+    var skip = ['logger', 'logger.js'];
+    while((skip.indexOf(modNm) >= 0) && trace.length > idx) {
+        var fpath = trace[idx].getFileName();
+        ++idx;
+        if(fpath.slice(0,'native'.length) == 'native') {
+            continue; // skip native modules
+        }
+        modNm = null;
+        var mod = null;
+        try {
+            mod = require(fpath);
+        } catch(err) {
+            // do nothing here, it's checked later
+        }
+        if(mod) { //__id is injected into services by the loader
+            if(!mod.__id) {
+                modNm = fpath.split('/').pop();
+                rmodNm = modNm;
+            } else {
+                modNm = mod.__id;
+                if(modNm != null) {
+                    rmodNm = modNm;
+                }
+            }
+        } else {
+            modNm = fpath.split('/').pop();
+            rmodNm = modNm;
+        }
     }
-    return null;
+    return rmodNm;
 }
 
 function setupTransports(cfg, logger) {
-
+    var func = 'setupTransports';
     //dynamically add transports based on the config
     //Each transport will have a package, which is the package name of the transport, e.g. winston-papertrail
     //and a field, which is the field within the transport containing the instance, e.g. Papertrail
     cfg.transports.forEach(function(transport) {
         transport.options = transport.options || {};
+        if(transport.options.timestamp) {
+            var ts = transport.options.timestamp.trim();
+            var tsl = ts.toLowerCase();
+            if(tsl == 'true') {
+                transport.options.timestamp = true;
+            } else if(tsl == 'false') {
+                transport.options.timestamp = false;
+            } else {
+                try {
+                    var tsm = require(global.__appDir + '/' + ts);
+                    if(typeof tsm.init === 'function' && typeof tsm.timestamp === 'function') {
+                        transport.options.timestamp = tsm.timestamp;
+                        tsm.init(); // allow one-time init if necessary, but require the function to be there
+                    } else {
+                        transport.options.timestamp = true;
+                        throw new Error('timestamp module must contain timestamp and init functions.');
+                    }
+                    try {
+                        console.log(func + ': type of transport.options.timestamp is now: ' + typeof tsm.timestamp);
+                    } catch(err) {
+                        console.log(func + ': error parsing timestamp type (true/false/modulename) - ' + err);
+                    }
+                } catch( err ) {
+                    //not sure, set it to true....
+                    console.log(func + ': defaulting timestamp to true. ' + err);
+                }
+            }
+        }
         var locationsToTry = [
             transport.package,
             path.join(global.__appDir, 'node_modules', transport.package),
@@ -169,3 +267,4 @@ function setupTransports(cfg, logger) {
     });
 
 }
+
