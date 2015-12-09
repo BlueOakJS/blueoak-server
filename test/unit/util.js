24f192503d95917d1aed0e9ee83c2381228d6049
Greg Considine <greg@greg-considine.com>
2015-12-09 15:06:05 -0500

    Expose test utility methods
    Exposing the existing test utility methods and adding injection
    for native services adds greater flexibility in testing
    sprout-server projects.  It can also reduce, or in some cases,
    eliminate the need to create mocks/stubs of Sprout services in
    order to test your local project.

--- test/unit/util.js
+++ test/unit/util.js
@@ -2,7 +2,8 @@
 //Some utilities to help with testing services
 
 var di = require('../../lib/di'),
-    _ = require('lodash')
+    _ = require('lodash'),
+    fs = require('fs'),
     sinon = require('sinon');
 
 //creates a mock config service
@@ -57,8 +58,60 @@ exports.initService = function(module, config, injections, callback) {
         callback();
     }
 
-}
+};
 
+/*
+ * This method lets you include unmodified Sprout services into your tests. One use case is 
+ * when testing your own service that depends on Sprout services, but the particular method
+ * you want to test does not.  Rather than replicating mocked/stubbed functionality that already
+ * exists in the native services, you can just inject the native services themselves so 
+ * init() and logic/calls within init() doesn't break.
+ *
+ * @param {Array|String} modules - An single name of a module or an array of module names
+ * @param {object} config - Your local config or mocked config as needed
+ * @param {function} callback - Called after all modules are required and init()'d. An object with 
+ *                              the mapping, `{ <module_name>: <module>, ... }` is returned.
+ */
+exports.injectNative = function (modules, config, callback) {
+  var nativeModules = fs.readdirSync(__dirname + '/../../services/'),
+      servicePattern = /^[a-z]+(.js)$/i,
+      initializedModules = {},
+      initializedCount = 0,
+      temp;
+
+  for(var i = 0; i < nativeModules.length; i++) {
+    if(!servicePattern.test(nativeModules[i])) {
+      nativeModules.splice(i, 1);  
+    }
+
+    nativeModules[i] = nativeModules[i].split('.')[0];
+  }
+
+  if(!(modules instanceof Array)) {
+    modules = [modules]; 
+  }
+
+  for(var i = 0; i < modules.length; i++) {
+    temp = {};
+
+    if(nativeModules.indexOf(modules[i]) === -1) {
+      throw new Error('Given module name is not a native service');
+    }
+
+    initializedModules[modules[i]] = require('../../services/' + modules[i]);
+
+    this.initService(initializedModules[modules[i]], config, function (error) {
+      if(error) {
+        throw new Error('Unable to inject native service');
+      }
+
+      initializedCount++;
+      if(initializedCount === modules.length) {
+        callback(initializedModules);
+      }
+    });
+  }
+};
 
 /* This is a new init method to replace initService
  * mod: the module to init
@@ -76,7 +129,7 @@ exports.init = function(mod, serviceMap, serviceCallback, callback) {
     
     global.services = {
       get: function(name) {
-          console.log("Get", name)
+          console.log("Get", name);
           return serviceMap[name];
       }  
     };
@@ -86,6 +139,7 @@ exports.init = function(mod, serviceMap, serviceCallback, callback) {
     paramNames.forEach(function(paramName) {
         if (!serviceMap[paramName]) {
             
+            console.log(__dirname);
             //since it's possible that this has already been loaded once and
             //has stubbed out methods, delete it from the cache
             var modPath = require.resolve('./mocks/' + paramName);
