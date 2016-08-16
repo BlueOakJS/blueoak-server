c2fbd380db20fffe188e8efa65d74737c4f7c3ca
Sean Kennedy <sean.kennedy@pointsource.com>
2016-08-15 21:50:15 -0400

    Add tests for swagger response model validation

--- test/integration/launchUtil.js
+++ test/integration/launchUtil.js
@@ -7,34 +7,38 @@ var path = require('path'),
 
 var lastLaunch = null;
 
-exports.launch = function(fixtureName, opts, done) {
+exports.launch = function (fixtureName, opts, done) {
 
     //opts is optional
     if (!done) {
         done = opts;
-        opts = {
-            exec: '../../bin/blueoak-server.js'
-        };
+        opts = {};
+    }
+    if (!opts.exec) {
+        opts.exec = '../../bin/blueoak-server.js';
     }
 
     var output = '';
     lastLaunch = child_process.execFile(path.resolve(__dirname, opts.exec), [],
-        {'cwd': path.resolve(__dirname, 'fixtures/' + fixtureName)},
-        function(err, stdout, stderr) {
+        {
+            'cwd': path.resolve(__dirname, 'fixtures/' + fixtureName),
+            'env': opts.env
+        },
+        function (err, stdout, stderr) {
             if (err) {
                 console.warn(err, stderr);
             }
             output += stdout + stderr;
         });
-    setTimeout(function() {
-        output = output.length > 50? output: null; //if output > 50, probably contains a stack tracegu
+    setTimeout(function () {
+        output = output.length > 50 ? output : null; //if output > 50, probably contains a stack tracegu
         done(output);
     }, 4000);
 };
 
-exports.finish = function(done) {
+exports.finish = function (done) {
     lastLaunch.kill('SIGINT');
-    setTimeout(function() {
+    setTimeout(function () {
         done();
     }, 2500);
 };
