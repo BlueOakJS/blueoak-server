f9f393d9719d0c197a631766dca7241ba0c9e038
shepp <ryshep111@gmail.com>
2016-10-10 14:23:55 -0400

    build apiKey extractor around RFC 7616. set consistent property on request object for all three auth schemes

--- middleware/bos-authentication.js
+++ middleware/bos-authentication.js
@@ -29,7 +29,7 @@ function init(app, config, logger, serviceLoader, swagger) {
                     if (operation['security']) {
                         _.keys(operation['security']).forEach(function (securityReq) {
                             _applySecurityRequirement(app, method, routePath, securityReq,
-                                api.securityDefinitions[securityReq], operation['x-bos-permissions'][securityReq],
+                                api.securityDefinitions[securityReq], /*operation['x-bos-permissions'][securityReq],*/
                                 operation['security'][securityReq]);
                         });
                     }
@@ -39,7 +39,8 @@ function init(app, config, logger, serviceLoader, swagger) {
     });
 }
 
-function _applySecurityRequirement(app, method, route, securityReq, securityDefn, requiredPermissions, requiredScopes) {
+function _applySecurityRequirement(app, method, route, securityReq,
+                                   securityDefn, /*requiredPermissions,*/ requiredScopes) {
     if (passportEnabled) {
         var passportService = loader.get('bosPassport');
         passportService.authenticate(securityReq);
@@ -101,46 +102,53 @@ function _applySecurityRequirement(app, method, route, securityReq, securityDefn
     }*/
 }
 
-function wrapAuthorizationMethod(authorizationMethod, route, securityDefn, requiredPermissions) {
+/*function wrapAuthorizationMethod(authorizationMethod, route, securityDefn, requiredPermissions) {
     return function (req, res, next) {
         var runTimeRequiredPermissions = _expandRouteInstancePermissions(requiredPermissions, route, req.path);
         authorizationMethod.call(this, req, res, next, securityDefn, runTimeRequiredPermissions);
     };
-}
+}*/
 
 function basicExtractor() {
     return function (req, res, next) {
         //if there is no auth header present, send back challenge 401 with www-authenticate header?
+            //now that I think about this, think this should be handled by user defined code
+            //because their is no way for us to know how to set the header fields (i.e. realm)
         //header should be of the form "Basic " + user:password as a base64 encoded string
         var authHeader = req.headers['Authorization'];
-        var credentialsBase64 = authHeader.substring(authHeader.indexOf('Basic ') + 1);
+        var credentialsBase64 = authHeader.substring(authHeader.split('Basic ')[1]);
         var credentials = base64URL.decode(credentialsBase64).split(':');
-        req.bos.user = {name: credentials[0], password: credentials[1], realm: 'Basic'};
+        req.bos.authenticationData = {username: credentials[0], password: credentials[1], scheme: 'Basic'};
         next();
     };
 }
 
-function apiKeyExtractor(securityDefn, verify) {
+function apiKeyExtractor(securityDefn) {
     //if there is no apiKey present, send back challenge 401 with www-authenticate header?
+        //now that I think about this, think this should be handled by user defined code
+        //because their is no way for us to know how to set the header fields (i.e. realm)
     return function (req, res, next) {
-        if (!verifyMD5Header(req.body, req.headers['Content-MD5'])) {
-            log.error('content md5 header for uri %s coming from %s did not match request body', req.path, req.ip);
-            res.status(401).send('content md5 header did not match request body');
-        }
-        var apiId = 'where do we get this from?';
-        var digest;
+        //should be form of username="Mufasa", realm="myhost@example.com"
+        var authorizationHeaders = req.headers['Authorization'].split(', ');
+        var authenticationData = {scheme: 'apiKey'};
+        authorizationHeaders.forEach(function (header) {
+            //should be form of username="Mufasa"
+            var keyValPair = header.split('=');
+            authenticationData[keyValPair[0]] = keyValPair[1].substring(1, keyValPair[1].length - 1);
+        });
         if (securityDefn.in === 'query') {
-            digest = req.query[securityDefn.name];
+            authenticationData.password = req.query[securityDefn.name];
         } else if (securityDefn.in === 'header') {
-            digest = req.headers[securityDefn.name];
+            authenticationData.password = req.headers[securityDefn.name];
         } else {
             return log.warn('unknown location %s for apiKey. ' +
                 'looks like open api specs may have changed on us', securityDefn.in);
         }
+        req.bos.authenticationData = authenticationData;
         //this would have to be a user provided function that
         //fetches the user (and thus the private key that we need to compute the hash) from some data source
         //we don't need this if we decide that we will let the user figure out how to verify the digest
-        verify(apiId, function (user) {
+        /*verify(apiId, function (user) {
             //regenerate hash with apiKey
             //hash will include symmetric apiKey, one or more of:
             //request method, content-md5 header, request uri, timestamp, socket.remoteAddress, req.ip, ip whitelist?
@@ -149,7 +157,7 @@ function apiKeyExtractor(securityDefn, verify) {
             // else you suck
             req.bosAuth.user = user;
             next();
-        });
+        });*/
     };
 }
 
@@ -165,19 +173,14 @@ function oauth2(route, securityDefn, scopes) {
     };
 }
 
-function verifyMD5Header(body, md5) {
-    //calculate md5 of request body and verify that it equals the header
-    return true;
-}
-
-function _expandRouteInstancePermissions(perms, route, uri) {
-    /* relate the route path parameters to the url instance values
+/*function _expandRouteInstancePermissions(perms, route, uri) {
+     relate the route path parameters to the url instance values
      perms: ["api:read:{policyid}", "api:read:{claimid}"]
      route: /api/v1/policies/:policyid/claims/:claimid
      [ api,v1,policies,:policyid,claims,:claimid ]
      uri:   /api/v1/policies/SFIH1234534/claims/37103
      [ api,v1,policies,SFIH1234534,claims,37103 ]
-     */
+
     if (!_.isString(route) ||  !_.isString(uri)) {
         return perms;
     }
@@ -201,7 +204,7 @@ function _expandRouteInstancePermissions(perms, route, uri) {
         });
         return ePerm;
     });
-}
+}*/
 
 //swagger paths use {blah} while express uses :blah
 function _convertPathToExpress(swaggerPath) {
