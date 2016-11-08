c130c24c57b2fd046e0ca7bd356f9d42e0a78a3f
ryshep111 <ryan.sheppard@pointsource.com>
2016-11-08 09:11:55 -0500

    Load middleware from node_modules (#80)
    * attempt to load middleware listed in the config that may be in node modules
    
    * test added for middleware node module load

--- test/integration/testLoader.js
+++ test/integration/testLoader.js
@@ -3,7 +3,8 @@
  * MIT Licensed
  */
 var assert = require('assert'),
-    util = require('./launchUtil');
+    util = require('./launchUtil'),
+    path = require('path');
 
 describe('SERVER6 - duplicate service name should fail on startup', function () {
     this.timeout(5000);
@@ -45,6 +46,36 @@ describe('SERVER10 - handler with invalid name should fail on startup', function
     it('Launch server and check for failure', function (done) {
         util.launch('server10', function(output) {
             assert.ok(output.indexOf('Names cannot contain periods') > -1);
+            done();
+        });
+    });
+});
+
+describe('SERVER10 - handler with invalid name should fail on startup', function () {
+    this.timeout(5000);
+
+    after(function (done) {
+        util.finish(done);
+    });
+
+    it('Launch server and check for failure', function (done) {
+        util.launch('server10', function(output) {
+            assert.ok(output.indexOf('Names cannot contain periods') > -1);
+            done();
+        });
+    });
+});
+
+describe.only('SERVER11 - middleware should get loaded from node modules', function () {
+    this.timeout(5000);
+
+    after(function (done) {
+        util.finish(done);
+    });
+
+    it('Launch server and load middleware', function (done) {
+        util.launch('server11', {appDir: path.resolve(__dirname, 'fixtures/server11')}, function(output) {
+            assert.ok(output === null);
             done();
         });
     });
