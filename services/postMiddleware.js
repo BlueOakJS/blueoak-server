472dc375f59115b0d5cb0daf6636adab5003020a
ryshep111 <ryan.sheppard@pointsource.com>
2017-01-18 21:07:38 -0500

    Also load post-middleware from node modules (#86)

--- services/postMiddleware.js
+++ services/postMiddleware.js
@@ -11,12 +11,24 @@
  * We always load the built-in error handler, middleware/_errors.js, last.
  * That way an app gets the opportunity to handle the error themselves first.
  */
+
+var _ = require('lodash');
+
 exports.init = function(config, serviceLoader, express, callback) {
 
     var cfg = config.get('express');
     var postMiddleware = cfg['middleware$'] || [];
     postMiddleware = postMiddleware.concat('_errors');
-
+    //this should catch middleware that exists in node modules instead of a /middleware directory
+    _.forEach(postMiddleware, function (middleware) {
+        if (!serviceLoader.getConsumer('middleware', middleware)) {
+            try {
+                serviceLoader.loadConsumerModules('middleware', [middleware]);
+            } catch (err) {
+                return callback(err);
+            }
+        }
+    });
     serviceLoader.initConsumers('middleware', postMiddleware, function initPostHandlerCallback(err) {
         callback(err);
     });
