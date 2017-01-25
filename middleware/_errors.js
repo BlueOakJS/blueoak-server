49e41eee6085ecbedfcc5c2b619ee2c3f1e062d3
Sean Kennedy <sean.kennedy@pointsource.com>
2017-01-25 01:53:02 -0500

    Update errors post-middleware to handle security

--- middleware/_errors.js
+++ middleware/_errors.js
@@ -5,20 +5,21 @@
 exports.init = function (app, logger) {
 
     app.use(function (err, req, res, next) {
+        var errorBody;
 
         //Validation errors are thrown by the swagger validator.
         //The can wrap a tv4 validation error which are passed under subErrors
         if (err.name === 'ValidationError') {
-            var payload = {
+            errorBody = {
                 message: err.message,
                 status: 422,
-                type: 'ValidationError'
+                type: err.name
             };
 
             if (err.subErrors) {
-                payload.validation_errors = [];
+                errorBody.validation_errors = [];
                 err.subErrors.forEach(function(subError) {
-                    payload.validation_errors.push({
+                    errorBody.validation_errors.push({
                         message: subError.message,
                         field: subError.dataPath,
                         schemaPath: subError.schemaPath,
@@ -26,8 +27,22 @@ exports.init = function (app, logger) {
                     });
                 });
             }
-            res.status(422).json(payload);
-        } else {
+        } else if (err.name === 'SecurityError') {
+            errorBody = {
+                message: err.message,
+                status: err.challenge ? 401 : 403,
+                type: err.name
+            };
+            
+            if (errorBody.status === 401) {
+                res.set('WWW-Authenticate', err.challenge);
+            }
+        }
+        
+        if (errorBody) {
+            res.status(errorBody.status || 500).send(errorBody);
+        }
+        else {
             logger.error(err.stack);
             res.status(500).send(err.message);
         }
