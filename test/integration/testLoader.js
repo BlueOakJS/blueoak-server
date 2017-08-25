7b00fe09c7b6ad72deae9984eeded5287b96997f
Adam Bennett <adam.bennett@pointsource.com>
2017-08-25 07:49:30 -0600

    Restore tests

--- test/integration/testLoader.js
+++ test/integration/testLoader.js
@@ -7,80 +7,80 @@ var request = require('request-promise'),
     util = require('./launchUtil'),
     path = require('path');
 
-// describe('SERVER6 - duplicate service name should fail on startup', function () {
-//     this.timeout(5000);
-//
-//     after(function (done) {
-//         util.finish(done);
-//     });
-//
-//     it('Launch server and check for failure', function (done) {
-//         util.launch('server6', function(output) {
-//             assert.ok(output.indexOf('already exists') > -1);
-//             done();
-//         });
-//     });
-// });
-//
-// describe('SERVER9 - service module with invalid name should fail on startup', function () {
-//     this.timeout(5000);
-//
-//     after(function (done) {
-//         util.finish(done);
-//     });
-//
-//     it('Launch server and check for failure', function (done) {
-//         util.launch('server9', function(output) {
-//             assert.ok(output.indexOf('Names cannot contain periods') > -1);
-//             done();
-//         });
-//     });
-// });
-//
-// describe('SERVER10 - handler with invalid name should fail on startup', function () {
-//     this.timeout(5000);
-//
-//     after(function (done) {
-//         util.finish(done);
-//     });
-//
-//     it('Launch server and check for failure', function (done) {
-//         util.launch('server10', function(output) {
-//             assert.ok(output.indexOf('Names cannot contain periods') > -1);
-//             done();
-//         });
-//     });
-// });
-//
-// describe('SERVER10 - handler with invalid name should fail on startup', function () {
-//     this.timeout(5000);
-//
-//     after(function (done) {
-//         util.finish(done);
-//     });
-//
-//     it('Launch server and check for failure', function (done) {
-//         util.launch('server10', function(output) {
-//             assert.ok(output.indexOf('Names cannot contain periods') > -1);
-//             done();
-//         });
-//     });
-// });
-//
-// describe('SERVER11 - middleware should get loaded from node modules', function () {
-//     this.timeout(5000);
-//
-//     after(function (done) {
-//         util.finish(done);
-//     });
-//
-//     it('Launch server and load middleware', function (done) {
-//         util.launch('server11', {appDir: path.resolve(__dirname, 'fixtures/server11')}, function(output) {
-//             assert.ok(output === null);
-//             done();
-//         });
-//     });
-// });
+describe('SERVER6 - duplicate service name should fail on startup', function () {
+    this.timeout(5000);
+
+    after(function (done) {
+        util.finish(done);
+    });
+
+    it('Launch server and check for failure', function (done) {
+        util.launch('server6', function(output) {
+            assert.ok(output.indexOf('already exists') > -1);
+            done();
+        });
+    });
+});
+
+describe('SERVER9 - service module with invalid name should fail on startup', function () {
+    this.timeout(5000);
+
+    after(function (done) {
+        util.finish(done);
+    });
+
+    it('Launch server and check for failure', function (done) {
+        util.launch('server9', function(output) {
+            assert.ok(output.indexOf('Names cannot contain periods') > -1);
+            done();
+        });
+    });
+});
+
+describe('SERVER10 - handler with invalid name should fail on startup', function () {
+    this.timeout(5000);
+
+    after(function (done) {
+        util.finish(done);
+    });
+
+    it('Launch server and check for failure', function (done) {
+        util.launch('server10', function(output) {
+            assert.ok(output.indexOf('Names cannot contain periods') > -1);
+            done();
+        });
+    });
+});
+
+describe('SERVER10 - handler with invalid name should fail on startup', function () {
+    this.timeout(5000);
+
+    after(function (done) {
+        util.finish(done);
+    });
+
+    it('Launch server and check for failure', function (done) {
+        util.launch('server10', function(output) {
+            assert.ok(output.indexOf('Names cannot contain periods') > -1);
+            done();
+        });
+    });
+});
+
+describe('SERVER11 - middleware should get loaded from node modules', function () {
+    this.timeout(5000);
+
+    after(function (done) {
+        util.finish(done);
+    });
+
+    it('Launch server and load middleware', function (done) {
+        util.launch('server11', {appDir: path.resolve(__dirname, 'fixtures/server11')}, function(output) {
+            assert.ok(output === null);
+            done();
+        });
+    });
+});
 
 describe('SERVER12 - single mock service should get loaded when specified by the --mock-services CLI argument',
     function () {
