4aa9d63b45bf197f5193137c164ff1f560a2cbba
Brant Wellons <brant.wellons@pointsource.com>
2018-02-20 13:20:45 -0500

    Polish

--- middleware/cors.js
+++ middleware/cors.js
@@ -19,10 +19,10 @@ exports.init = function(app, config, logger, callback) {
     	if (!cfg.origin) {
     		cfg.origin = regex;
     	} else if (_.isArray(cfg.origin)) {
-    		cfg.origin.push(regex)
+    		cfg.origin.push(regex);
     	}
     }
-    
+
     app.use(cors(cfg));
     logger.debug('Enabled CORS.');
     callback();
